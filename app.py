c998886edc4ab87a0fa59fab89d45e0043a56ed5
Shah <urvi.a.shah@capgemini.com>
2022-10-06 11:45:02 +0530

    app.py

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ import streamlit as st
 import webbrowser
 import numpy as np
 import pandas as pd
-import matplotlib.pyplot as plt
+# import matplotlib.pyplot as plt
 import seaborn as sns
 import os
 from mpl_toolkits.mplot3d import Axes3D
